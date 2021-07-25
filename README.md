ef3c134b0ac593c4acb185cb47961591c7088d61
Filip Szczerek <ga.software@yahoo.com>
2021-07-25 13:15:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# libspinnaker-sys - bindings to the Spinnaker library
+
+https://www.flir.com/products/spinnaker-sdk/
+
+
+## License
+
+Licensed under MIT license. See the LICENSE file for details.
+
+
+## Usage
+
+Set `SPINNAKER_LIBDIR` environment variable to specify the location of Spinnaker libraries. This is optional under Linux and alikes, but required under Windows (example value: "C:\Program Files\FLIR Systems\Spinnaker\bin64\vs2015").
---
+++ build.rs
@@ -0,0 +1,66 @@
+use std::env;
+use std::path::Path;
+
+fn main() {
+    let target = env::var("TARGET").expect("getting target");
+
+    let libdir = match env::var("SPINNAKER_LIBDIR") {
+        Ok(dir) => dir,
+        Err(_) => {
+            match target.as_ref() {
+                "x86_64-pc-windows-msvc"
+				| "x86_64-pc-windows-gnu"
+				| "i686-pc-windows-msvc"
+				| "i686-pc-windows-gnu" => panic!("Missing env. variable SPINNAKER_LIBDIR. Set it to Spinnaker_C DLL location."),
+				
+                _ => "/opt/spinnaker/lib".to_string(),
+            }
+        }
+    };
+
+    let libdir = Path::new(&libdir);
+
+    let libname = match target.as_ref() {
+        "x86_64-pc-windows-msvc"
+		| "i686-pc-windows-msvc"
+		| "x86_64-pc-windows-gnu"
+		| "i686-pc-windows-gnu" => find_windows_library_in_dir(&libdir),
+		
+        _ => "Spinnaker_C".to_string(),
+    };
+
+    println!("cargo:rustc-link-search=native={}", libdir.display());
+    println!("cargo:rustc-link-lib={}", libname);
+}
+
+fn find_windows_library_in_dir(path: &Path) -> String {
+    let mut dlls = vec![];
+
+    for d_entry in std::fs::read_dir(path).unwrap() {
+        if d_entry.is_err() { continue; }
+        if let Ok(fname) = d_entry.unwrap().file_name().into_string() {
+            dlls.push(fname);
+        }
+    }
+
+    let stem = "SpinnakerC";
+
+    let get_stem = |s| { return Path::new(s).file_stem().unwrap().to_str().unwrap().to_string(); };
+
+    // if the user took care to rename one of the versioned DLLs as "SpinnakerC.dll", use it
+    if let Some(item) = dlls.iter().find(|s| **s == format!("{}.dll", stem)) {
+        return get_stem(item);
+    }
+
+    // otherwise, use the first non-debug one
+    if let Some(item) = dlls.iter().find(|s| s.starts_with(&format!("{}_", stem)) && s.ends_with(".dll")) {
+        return get_stem(item);
+    }
+
+    // otherwise, use the first debug one
+    if let Some(item) = dlls.iter().find(|s| s.starts_with(&format!("{}d", stem)) && s.ends_with(".dll")) {
+        return get_stem(item);
+    }
+
+    panic!("Could not find SpinnakerC*.dll in {}.", path.display());
+}
